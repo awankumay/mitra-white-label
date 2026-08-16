5dbc8c37058b9a0058a8e281f5fe08e493dca7df
awankumay <andika.pris2@gmail.com>
2026-08-16 23:00:33 +0800

    feat: register ContextServiceProvider and bind context contracts (TODO 5.2, ADR-009)

--- core/Config/core.php
+++ core/Config/core.php
@@ -2,12 +2,12 @@
 
 return [
     'providers' => [
-        // Sub-provider Core didaftarkan di sini saat domain dibuat,
-        // contoh: Core\Organization\OrganizationServiceProvider::class.
+        Core\Context\ContextServiceProvider::class,
     ],
     'organization' => [
         'max_depth' => 10,
     ],
-    // Domain sub-sistem ditambahkan saat milestone yang membutuhkannya,
-    // misal: 'context' => [...], 'settings' => [...]
+    'context' => [
+        'session_key' => 'context.unit_id',
+    ],
 ];
